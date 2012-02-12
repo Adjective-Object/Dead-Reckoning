b86c922c253f053d2185e54e594e8ecec274b133
PurpleTengu <mhuan13@gmail.com>
2012-02-11 19:08:01 -0500

    fixed up the save system - yet to finalize (ceate save and load methods for all Entities). NOTABLE ERROR:  causes crash on failed entity load.

---
+++ src/net/plaidypus/deadreckoning/entities/Statue.java
@@ -0,0 +1,69 @@
+package net.plaidypus.deadreckoning.entities;
+
+import java.util.ArrayList;
+
+import net.plaidypus.deadreckoning.actions.Action;
+import net.plaidypus.deadreckoning.actions.WaitAction;
+import net.plaidypus.deadreckoning.board.GameBoard;
+import net.plaidypus.deadreckoning.board.Tile;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+public class Statue extends Entity{
+	
+	static Image i;
+	
+	//Exists only for the purpose of referencing methods that should be static,
+	// but need to be abstract, because fuck Java
+	public Statue(){} 
+	
+	public Statue(Tile t, int layer) {
+		super(t, layer);
+	}
+	
+	public static void init() throws SlickException{
+		 i = new Image("res/statue.png");
+	}
+	
+	@Override
+	public void update(GameContainer gc, int delta) {}
+
+	@Override
+	public void updateBoardEffects(GameContainer gc, int delta) {}
+
+	@Override
+	public Action chooseAction(GameContainer gc, int delta) {return new WaitAction(this);}
+
+	@Override
+	public void forceRender(Graphics g, float x, float y) {
+		g.drawImage(i,x,y);
+	}
+
+	@Override
+	public boolean isInteractive() {
+		return false;
+	}
+
+	@Override
+	public ArrayList<Action> advanceTurn() {
+		return null;
+	}
+
+	@Override
+	public Entity makeFromString(GameBoard target, String[] attributes) {
+		System.out.println(" "+attributes[0]+" "+attributes[1]+" "+attributes[2]);
+		return new Statue(target.getTileAt(Integer.parseInt(attributes[1]),Integer.parseInt(attributes[2])), Integer.parseInt(attributes[2]));
+	}
+
+	@Override
+	public String saveToString() {
+		return getGenericSave();
+	}
+
+	@Override
+	public void onDeath() {}
+
+}
